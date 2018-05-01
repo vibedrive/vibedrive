335b6733268811a21291c55cac03e57ae720cb59
kareniel <jonathand@campsiteproject.com>
2018-05-01 13:24:44 -0400

    dark layout & font

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Files</router-link>
-    </div>
-    <router-view/>
-  </div>
+
+  <v-app id="app" dark>
+    <v-navigation-drawer
+      clipped
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+
+      <router-view/>
+
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+  #app
+    font-family: 'Work Sans', sans-serif
+    font-weight: 500
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+  #nav
+    padding 30px
+    a
+      font-weight bold
+      color #2c3e50
+      &.router-link-exact-active
+        color #42b983
 </style>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: true
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@/assets/fonts/WorkSans.css'
 
 import App from './App.vue'
 import router from './router'
