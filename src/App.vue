5095790a4e868d36ac1868dfa3c5af3a90c7f9f9
kareniel <jonathand@campsiteproject.com>
2018-05-01 13:41:15 -0400

    playlist-menu

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
 
   <v-app id="app" dark>
-    <v-navigation-drawer
-      clipped
-      fixed
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+    <v-navigation-drawer clipped fixed v-model="drawer" app>
+
+        <PlaylistsMenu></PlaylistsMenu>
+        
+
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
@@ -58,12 +39,17 @@
 </style>
 
 <script>
+  import PlaylistsMenu from '@/components/PlaylistsMenu'
+
   export default {
     data: () => ({
       drawer: true
     }),
     props: {
       source: String
+    },
+    components: {
+      PlaylistsMenu
     }
   }
 </script>
---
+++ src/components/PlaylistsMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-list>
+    <v-list-group
+      v-for="group in groups"
+      v-model="group.active"
+      :key="group.title"
+      :prepend-icon="group.action"
+      no-action>
+
+      <v-list-tile slot="activator">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ group.title }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+
+      <v-list-tile v-for="subItem in group.items" :key="subItem.title" @click="">
+
+        <v-list-tile-action>
+          <v-icon>star</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ subItem.title }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+          <v-icon></v-icon>
+        </v-list-tile-action>
+        
+      </v-list-tile>
+    </v-list-group>
+
+  </v-list>
+</template>
+
+<script>
+  export default {
+    name: 'PlaylistsMenu',
+    props: {
+      
+    },
+    data: () => ({
+      groups: [{
+        active: true,
+        title: 'Group',
+        items: [
+          { title: 'Item A' }, 
+          { title: 'Item B' }
+        ]
+      }]
+    })
+  }
+</script>
+
